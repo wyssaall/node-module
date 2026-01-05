f9a67d47bfa258623a4dd2ac22cb93e564089f02
Wissal <wissal1zabour@gmail.com>
2026-01-05 14:06:35 +0100

    export et require math

---
+++ app.js
@@ -0,0 +1,3 @@
+const math = require('./math.js');
+console.log(math.add(5, 3));
+console.log(math.sub(9, 3));
---
+++ math.js
@@ -0,0 +1,20 @@
+function add(a,b){
+    let sum;
+    sum = a + b;
+    return sum;
+}
+
+
+function sub(a,b){
+    let subs;
+    subs = a - b;
+    return subs;
+}
+
+function mult(a,b){
+    let multi;
+    multi = a * b;
+    return multi;
+}
+
+module.exports = {add,sub};
